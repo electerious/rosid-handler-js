a6d1127d415d6c73893c8b9ef93024546c0c2486
Tobias Reich <tobias.reich.ich@gmail.com>
2015-12-27 22:16:35 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let fs       = require('fs'),
+    async    = require('async'),
+    babel    = require('./lib/babel'),
+    uglifyjs = require('./lib/uglifyjs')
+
+/*
+ * Load, transform and compress JS.
+ * @param {string} filePath - Absolute path to the requested file.
+ * @param {string} srcPath - Absolute path to the source folder.
+ * @param {string} distPath - Absolute path to the export folder.
+ * @param {object} route - The route which matched the request URL.
+ * @param {function} next - The callback that handles the response. Receives the following properties: err, result, savePath.
+ */
+module.exports = function(filePath, srcPath, distPath, route, next) {
+
+	let savePath = filePath.replace(srcPath, distPath)
+
+	async.waterfall([
+
+		(next)      => fs.readFile(filePath, 'utf8', next),
+		(str, next) => babel(str, next),
+		(str, next) => uglifyjs(str, next)
+
+	], (err, str) => {
+
+		if (err!=null) {
+			next(err, null, null)
+			return false
+		}
+
+		next(null, str, savePath)
+
+	})
+
+}
