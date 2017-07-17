665121ef059963792d4ede448f6db4e7efd88d95
Tobias Reich <tobias.reich.ich@gmail.com>
2017-07-17 10:08:30 +0200

    Support for next version of Rosid

--- src/index.js
+++ src/index.js
@@ -62,10 +62,10 @@ module.exports.out = function(opts) {
 
 /**
  * Attach an array to the function, which contains a list of
- * extensions used by the handler. The array will be used by Rosid for caching purposes.
+ * file patterns used by the handler. The array will be used by Rosid for caching purposes.
  * @public
  */
 module.exports.cache = [
-	'.js',
-	'.json'
+	'**/*.js',
+	'**/*.json'
 ]
